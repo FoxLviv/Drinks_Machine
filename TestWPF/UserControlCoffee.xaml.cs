b9183cdc2fc10b1fb55a2c3770064ad2ee53700f
Andriy Burtso <burco.ab@gmail.com>
2019-07-01 23:49:57 +0300

    created main window and some user frameworks

---
+++ TestWPF/UserControlCoffee.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControlCoffee.xaml
+    /// </summary>
+    public partial class UserControlCoffee : UserControl
+    {
+        public UserControlCoffee()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewOptions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewOptions.SelectedIndex;
+            
+            switch(index)
+            {
+                case 0:
+                    break;
+                default:
+                    break;
+                    
+            }
+        }
+    }
+}
